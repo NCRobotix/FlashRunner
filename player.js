1faefc0446a217afccdcf8a389c70ab9f7a7ff0d
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-04-24 17:35:51 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,16 @@
-class Player{
+export class Player{
   constructor(game){
     this.game = game;
+    this.width = 100;
+    this.heigh = 100;
+    this.x = 0;
+    this.y = 100
+  }
+  update(){
     
+  }
+
+  draw(context){
+    context.fillRect(this.x, this.y, this.width, this.height);
   }
 }
