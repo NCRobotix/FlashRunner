7faa4349a7f2bf57557fdfd860b3cb7c4967f93f
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-04-24 14:35:39 -0400

    Update work.js

--- work.js
+++ work.js
@@ -1,1 +1,30 @@
 
+//////variables
+var stage = 0;
+
+var p1x = 400;
+var p1y = 375;
+var pWidth = 30;
+var pHeight = 70;
+
+var b1x = 200;
+var b1y = 300;
+var bWidth = 30;
+var bHeight = 70;
+
+function setUp(){
+  createCanvas(800, 500);
+  rectMode(center);
+  textAlign(center);
+}
+
+function draw(){
+  if(stage == 1){
+    game();
+  }
+}
+
+//////game function
+function game(){
+  background(20, 20, 20);
+}
