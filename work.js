ebbf452da0adbb13f8172f437a2785dd008e9e64
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-04-24 18:02:20 -0400

    Update work.js

--- work.js
+++ work.js
@@ -1,4 +1,4 @@
-import { Player } from './player.js';
+//import { Player } from './player.js';
 
 //////loading
 window.addEventListener('load', function(){
@@ -32,6 +32,25 @@ window.addEventListener('load', function(){
 )
 };
 
+class Player{
+  constructor(game){
+    this.game = game;
+    this.width = 100;
+    this.heigh = 100;
+    this.x = 0;
+    this.y = this.game.height - this.height;
+    this.iamge = document.getElementById('player');
+  }
+  update(){
+    
+  }
+
+  draw(context){
+    context.fillStyle = "red";
+    context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, this.x, this.y);
+  }
+}
 
 //////variables
 var stage = 0;
