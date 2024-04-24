190bdebaaacebb704f5029806741aa2aa0ccff0c
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-04-24 17:55:19 -0400

    Update player.js

--- player.js
+++ player.js
@@ -4,7 +4,8 @@ export class Player{
     this.width = 100;
     this.heigh = 100;
     this.x = 0;
-    this.y = 100
+    this.y = this.game.height - this.height;
+    this.iamge = document.getElementById('player');
   }
   update(){
     
@@ -13,5 +14,6 @@ export class Player{
   draw(context){
     context.fillStyle = "red";
     context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, this.x, this.y);
   }
 }
