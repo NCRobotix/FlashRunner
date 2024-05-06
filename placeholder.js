aa34296eff020b5628d8206a6df06a8f2f719ad8
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-05-05 23:22:08 -0400

    Update placeholder.js

--- placeholder.js
+++ placeholder.js
@@ -1,6 +1,7 @@
 window.onload = function draw() {
         var canvas = document.getElementById("gameCanvas");
         var ca = canvas.getContext("2d");
+        console.log(ca);
         ca.font = "50px Arial";
         ca.fillText("FLASH RUNNER", 10, 80);
       }
