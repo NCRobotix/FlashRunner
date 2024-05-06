0dd1d45186e368bf26c6d28a5d8da8689257ff79
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-05-05 20:31:18 -0400

    Create placeholder.js

---
+++ placeholder.js
@@ -0,0 +1,6 @@
+window.onLoad = function draw() {
+        let canvas = document.getElementById("gameCanvas");
+        let ca = canvas.getContext("2d");
+        ca.font = "50px Arial";
+        ca.fillText("FLASH RUNNER",10,80);
+      }
